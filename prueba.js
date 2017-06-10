f1c425ee91a273aea0ebbd5a1e925b5f06ff9905
komboty <komubk@gmail.com>
2017-06-09 22:51:43 -0500

    dos

--- prueba.js
+++ prueba.js
@@ -1,33 +1,21 @@
 new (function() {
     var ext = this;
-
-    // Cleanup function when the extension is unloaded
+    
     ext._shutdown = function() {};
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
 
-    // Functions for block with type 'w' will get a callback function as the 
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
-    ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
+    ext.p = function() {
+	hola
     };
 
-    // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['w', 'wait for random time', 'wait_random'],
+            ['w', 'prueba', 'p'],
         ]
     };
 
-    // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
+    ScratchExtensions.register('hola', descriptor, ext);
 })();
