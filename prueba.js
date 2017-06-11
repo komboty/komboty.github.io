38e15010e748b773b1bdaa1ac1358017c1e65282
komboty <komubk@gmail.com>
2017-06-11 12:30:37 -0500

    ya

--- prueba.js
+++ prueba.js
@@ -1,3 +1,5 @@
+var cadena = " ";
+
 (function() {
 	    
     	this._shutdown = function() {};
@@ -5,45 +7,42 @@
     	this._getStatus = function() {
         	return {status: 2, msg: 'Ready'};
     	};
-
    
 	
-	this.holi =  function(nombre){
-		cadena = "Neta";
-		if(nombre == "javier"){
-		console.log("Holi");
-		return cadena;
-		}else{
-			return "usuario no autorizado";
-		}
-		
-		
+	this.derecha =  function(){
+		bloque = "derecha";
+		cadena = cadena + bloque + " ";
+	};
+	
+	this.izquierda =  function(){
+		bloque = "izquierda";
+		cadena = cadena + bloque + " ";
 	};
 
-	this.secuencia1 = function(algo){
-		if(algo == 3){
-			alert("entro al if");
-			console.log("bien mi chavo");
-			var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+	this.imprimir = function(){				
+		return cadena;
+	};
 
-            saveAs(blob, "hello world.txt");
-		}else{
-			console.log("mal mi chavo");
-		}
+	this.borrar = function(){		
+		cadena = " ";
 	};
 
 	this.comenzar = function(){
-		return;
+		return true;
 	};
     
     	var descriptor = {
         	blocks: [
            
-			['r','hello world+ %s', 'holi','nombre'],
-			['w', 'Secuencia 1 %n', 'secuencia1','algo'],
+			[' ','Derecha', 'derecha'],
+			[' ','Izquierda', 'izquierda'],
+			['r', 'Imprimir', 'imprimir'],
+			[' ', 'Borrar', 'borrar'],
 			['h', 'Comenzar', 'comenzar']
         	]
     	};
     
+    ScratchExtensions.register('Pruebas', descriptor, this);
+})({});
     ScratchExtensions.register('Pruebas', descriptor, this);
 })({});
