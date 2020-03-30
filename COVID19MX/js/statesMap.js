f341471945ba871e8585640d29dc034cf6af5477
Alberto Salvador <komubk@gmail.com>
2020-03-30 04:21:31 -0600

    Add files via upload

--- COVID19MX/js/statesMap.js
+++ COVID19MX/js/statesMap.js
@@ -1,10 +1,10 @@
-let grades = [0, 5, 10, 15, 20, 25, 50, 100];
+let grades = [0, 5, 10, 20, 50, 100, 150, 200];
 let eOld;
 
 // -----------------------------------------------------------------
 // Configuracion del mapa
 // -----------------------------------------------------------------
-let map = L.map('map').setView([24.363, -100.749], 5);
+let map = L.map('map', { zoomControl: false }).setView([25.150, -100.749], 5);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
     maxZoom: 18
@@ -19,15 +19,21 @@ let geojson = L.geoJson(statesData, {
 // -----------------------------------------------------------------
 // Informacion del estado seleccionado
 // -----------------------------------------------------------------
-let info = L.control();
+let info = L.control({ position: 'topleft' });
 info.onAdd = function (map) {
     this._div = L.DomUtil.create('div', 'info');
-    this.update();    
+    this.update();
     return this._div;
 };
 info.update = function (props) {
-    this._div.innerHTML = '<h4>Confirmados con COVID-19</h4>Actualizado: ' + statesData.fecha
-        + (props ? '' : '<br><br>Selecciona un estado para ver información');
+    // this._div.innerHTML = '<h4>Confirmados con COVID-19 ' 
+    //     + (props ? '<br>en ' + props.estado : '')
+    //     + '</h4>Actualizado: ' +statesData.fecha
+    //     + (props ? '' : '<br><br>Selecciona un estado para ver más información');
+    this._div.innerHTML = '<h4>'
+        + (props ? props.name : '')
+        + '</h4>Actualizado: ' + ObjAllsOnes.fecha
+        + (props ? '' : '<br><br>Selecciona un estado para ver más información');
 };
 info.addTo(map);
 
@@ -51,14 +57,36 @@ legend.addTo(map);
 
 // Estilo de cada estado
 function style(feature) {
+    let city = feature.properties.name;
     return {
-        fillColor: getColor(feature.properties.confirmados),
+        fillColor: getColor(ObjAllsOnes[keyAllsOnes + keyOne][city][keyConfirm]),
         weight: 2,
         opacity: 1,
         color: 'white',
         dashArray: '3',
         fillOpacity: 0.7
     };
+}
+
+// Estilo de cada estado segun la llave de properties
+function styleByKey(key, value) {
+    geojson.clearLayers();
+    geojson = L.geoJson(statesData, {
+        /*style: function (feature) {
+            let city = feature.properties.name;
+            let keyAllsOnes = value || key;
+            return {
+                fillColor: getColor(ObjAllsOnes[keyAllsOnes + keyOne][city][keyConfirm]),
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7
+            }
+        }*/ style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+    //resertTable();
 }
 
 // Color correspondiente al numero de datos
@@ -101,13 +129,14 @@ function highlightFeature(e) {
 
     info.update(layer.feature.properties);
     updateTable(layer.feature.properties);
+    //displayEstado(true);
 }
 
 // Evento mouseout
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
     info.update();
-    resertTable();
+    //resertTable();
 }
 
 function onlyClick(e) {
--- COVID19MX/js/tableProps.js
+++ COVID19MX/js/tableProps.js
@@ -1,59 +1,156 @@
-let estadoTxt = document.getElementById('estadoT');
-
-let listTab = document.getElementById('list-tab');
-
+// identificadores
+let keyConfirm = 'confirmados';
+let keyMujer = 'mujer';
+let keyHombre = 'hombre';
 let keyEdad = 'edad';
-let rangosEdad = Object.keys(statesData.features[0].properties.edad);
-listTab.innerHTML += getBrGroupItemHTML();
-for (let index = 0; index < rangosEdad.length; index++) {
-    listTab.innerHTML += getGroupItemHTML(keyEdad, rangosEdad[index]);
-}
-
 let keyProce = 'procedencia';
-let arrayProce = Object.keys(statesData.features[0].properties.procedencia);
-listTab.innerHTML += getBrGroupItemHTML();
+
+let keyAll = 'All';
+let keyOne = 'One';
+let keyAllsOnes = keyConfirm;
+
+let idMenu = '-menu-vistas';
+let idSubMenu = '-sub' + idMenu;
+
+let idList = '-list';
+let idItmeList = '-item' + idList;
+
+
+// let allList = document.getElementById('info' + idList);
+// let estadoItemList = document.getElementById('estado' + idItmeList);
+
+// --------------------------------------------------------------------
+// Elements menu vistas 
+let titleDd = document.getElementById('title' + idMenu);
+let edadSubMenu = document.getElementById(keyEdad + idSubMenu);
+let proceSubMenu = document.getElementById(keyProce + idSubMenu);
+
+// --------------------------------------------------------------------
+// Elements lista general
+//let generalList = document.getElementById('general' + idList);
+
+let totalItemList = document.getElementById(keyConfirm + idItmeList);
+let numTotal = document.getElementById(keyConfirm + 'T');
+let barTotal = document.getElementById('bar' + keyConfirm + 'T');
+
+// --------------------------------------------------------------------
+// Elements lista sexo
+let sexoList = document.getElementById('sexo' + idList);
+
+let mujerItemList = document.getElementById(keyMujer + idItmeList);
+let numMujeres = document.getElementById(keyMujer + 'T');
+let barMujeres = document.getElementById('bar' + keyMujer + 'T');
+
+let hombresItemList = document.getElementById(keyHombre + idItmeList);
+let numHombres = document.getElementById(keyHombre + 'T');
+let barHombres = document.getElementById('bar' + keyHombre + 'T');
+
+// --------------------------------------------------------------------
+// Elements lista edad
+let edadList = document.getElementById(keyEdad + idList);
+
+// --------------------------------------------------------------------
+// Elements lista procedencia
+let procelList = document.getElementById(keyProce + idList);
+
+// --------------------------------------------------------------------
+// Creacion de items de la lista edad
+let rangosEdad = Object.keys(ObjAllsOnes[keyConfirm + keyAll][keyEdad]);
+//edadList.innerHTML += crearBrItemList();
+for (let index = 0; index < rangosEdad.length; index++) {
+    edadList.innerHTML += crearItemList(keyEdad, rangosEdad[index]);
+    edadSubMenu.innerHTML += crearItmeSubMenu(keyEdad, rangosEdad[index]);
+}
+
+// --------------------------------------------------------------------
+// Creacion de items de la lista procedencia
+let arrayProce = Object.keys(ObjAllsOnes[keyConfirm + keyAll][keyProce]);
+//procelList.innerHTML += crearBrItemList();
 for (let index = 0; index < arrayProce.length; index++) {
-    listTab.innerHTML += getGroupItemHTML(keyProce, arrayProce[index]);
+    procelList.innerHTML += crearItemList(keyProce, arrayProce[index]);
+    proceSubMenu.innerHTML += crearItmeSubMenu(keyProce, arrayProce[index]);
 }
 
-let numTotal = document.getElementById('confirmadosT');
-let barTotal = document.getElementById('barconfirmadosT');
-let numMujeres = document.getElementById('mujerT');
-let barMujeres = document.getElementById('barmujerT');
-let numHombres = document.getElementById('hombreT');
-let barHombres = document.getElementById('barhombreT');
-clickItem();
+// --------------------------------------------------------------------
+// Valores iniciales de la tabla
+initDataAllTable(keyAllsOnes);
+
+// --------------------------------------------------------------------
+// Control de Dropdwon de vistas
+$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
 
 
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+        $('.dropdown-submenu .show').removeClass("show");
+    });
+
+
+    return false;
+});
+
+
+// Actualiza los datos de la tabla segun la llave
+function initDataAllTable(key) {
+    let dataCity = ObjAllsOnes[key + keyAll];
+    /* setValorItemList(numTotal, 5, barTotal, dataCity.confirmados);
+    setValorItemList(numMujeres, 6, barMujeres, dataCity.mujer);
+    setValorItemList(numHombres, 6, barHombres, dataCity.hombre);
+    setValorEN(keyEdad, rangosEdad, dataCity.edad);
+    setValorEN(keyProce, arrayProce, dataCity.procedencia); */
+    setValorLists(dataCity[keyConfirm], dataCity[keyHombre], dataCity[keyMujer], dataCity[keyEdad], dataCity[keyProce]);
+}
+
+// Actualizacion de la tabla segun el estado seleccionado
 function updateTable(props) {
-    console.log(props);
-    setHElement(estadoTxt, props.estado, 5);
-    setValorGroupItem(numTotal, 5, barTotal, props.confirmados);
-    setValorGroupItem(numMujeres, 6, barMujeres, props.mujer);
-    setValorGroupItem(numHombres, 6, barHombres, props.hombre);
-    setValorEN(keyEdad, rangosEdad, props.edad);
-    setValorEN(keyProce, arrayProce, props.procedencia);
+    let dataCity = ObjAllsOnes[keyAllsOnes + keyOne][props.name];
+    //setHElement(estadoItemList, props.estado, 5);
+    /* setValorItemList(numTotal, 5, barTotal, dataCity.confirmados);
+    setValorItemList(numMujeres, 6, barMujeres, dataCity.mujer);
+    setValorItemList(numHombres, 6, barHombres, dataCity.hombre);
+    setValorEN(keyEdad, rangosEdad, dataCity.edad);
+    setValorEN(keyProce, arrayProce, dataCity.procedencia); */
+    setValorLists(dataCity[keyConfirm], dataCity[keyHombre], dataCity[keyMujer], dataCity[keyEdad], dataCity[keyProce]);
 }
 
+// Restablece la tabla a su configuracion inicial
 function resertTable() {
-    setHElement(estadoTxt, 'Estado', 5);
-    setValorGroupItem(numTotal, 5, barTotal, 0);
-    setValorGroupItem(numMujeres, 6, barMujeres, 0);
-    setValorGroupItem(numHombres, 6, barHombres, 0);
+    //setHElement(estadoItemList, 'Estado', 5);
+    /* setValorItemList(numTotal, 5, barTotal, 0);
+    setValorItemList(numMujeres, 6, barMujeres, 0);
+    setValorItemList(numHombres, 6, barHombres, 0);
     setValorEN(keyEdad, rangosEdad);
-    setValorEN(keyProce, arrayProce);
+    setValorEN(keyProce, arrayProce); */
+    setValorLists(0, 0, 0);
 }
 
-function setValorGroupItem(elementNumero, h, elementBar, valor) {
+// Inserta valores a la tabla
+function setValorLists(intConfirm, intHombres, intMujeres, intsEdad, intsProce) {
+    setValorItemList(numTotal, 5, barTotal, intConfirm);
+    setValorItemList(numMujeres, 6, barMujeres, intMujeres);
+    setValorItemList(numHombres, 6, barHombres, intHombres);
+    setValorEN(keyEdad, rangosEdad, intsEdad);
+    setValorEN(keyProce, arrayProce, intsProce);
+}
+
+// Asigna informacion a un item de la tabla
+function setValorItemList(elementNumero, h, elementBar, valor) {
     setHElement(elementNumero, valor, h);
-    elementBar.style.width = valor + '%';
-    elementBar.setAttribute("aria-valuenow", valor)
+    let porcentaje = valor / 2;
+    elementBar.style.width = porcentaje + '%';
+    elementBar.setAttribute("aria-valuenow", porcentaje)
 }
 
+// Crea un <h></h>
 function setHElement(element, valor, h) {
     element.innerHTML = '<h' + h + '>' + valor + '</h' + h + '>';
 }
 
+// Asigna informacion a un grupo de items de la tabla
 function setValorEN(keyElement, ObjKeys, obj) {
     let numElement, barElement, key, valor;
 
@@ -63,69 +160,93 @@ function setValorEN(keyElement, ObjKeys, obj) {
         numElement = document.getElementById(keyElement + ' ' + key + 'T');
         barElement = document.getElementById('bar' + keyElement + ' ' + key + 'T');
 
-        setValorGroupItem(numElement, 6, barElement, valor);
+        setValorItemList(numElement, 6, barElement, valor);
     }
 }
 
-function clickItem() {
-    $('#list-tab a').on('click', function (e) {
-        // e.preventDefault()
-        // $(this).tab('show')
+// Click en boton de vistas
+function clickView(key, value) {
+    let titulo = key == keyConfirm ? 'todos' : key;
+    let subTitulo = value ? '> ' + value + ' ' : '';
+    let icono = '<i class="fa fa-bars"></i>';
+    let txtBtn = ' Confirmados > ' + titulo + ' ' + subTitulo;
+    titleDd.innerHTML = icono + txtBtn.toUpperCase();
+    keyAllsOnes = value || key;
 
-        let row = e.currentTarget.firstElementChild;
-        if (row) {
-            console.log(row.lastElementChild.id);
-            let idSinT = row.lastElementChild.id.slice(0, -1);
-            let idSplit = idSinT.split(' ');
-
-            if (idSplit.length > 2) {
-                idSplit[1] = idSplit[1] + ' ' + idSplit[2];
-            }
-            styleByKey(idSplit[0], idSplit[1]);
-        }
-    })
+    styleByKey(key, value);
+    info.update();    
+    initDataAllTable(keyAllsOnes);
+    displayListsBy(key);
+    //displayEstado();
 }
 
-function styleByKey(key, value) {
-    geojson.clearLayers();
-    geojson = L.geoJson(statesData, {
-        style: function (feature) {
-            let color = feature.properties[key];
-            if (value) {
-                color = color[value];
-            }
-            
-            return {
-                fillColor: getColor(color),
-                weight: 2,
-                opacity: 1,
-                color: 'white',
-                dashArray: '3',
-                fillOpacity: 0.7
-            }
-        },
-        onEachFeature: onEachFeature
-    }).addTo(map);
-    resertTable();
+// Oculta o muestra items de la tabla
+function displayListsBy(key) {
+    resetDisplayList();
+
+    switch (key) {
+        case keyConfirm:
+            break;
+
+        case keyMujer:
+        case keyHombre:
+            sexoList.style.display = 'none';
+            break;
+
+        case keyEdad:
+            edadList.style.display = 'none';
+            break;
+
+        case keyProce:
+            procelList.style.display = 'none';
+            break;
+    }
 }
 
-function getBrGroupItemHTML() {
-    return '<a class="list-group-item list-group-item-light"></a>';
+function resetDisplayList() {
+    sexoList.style.display = '';
+    edadList.style.display = '';
+    procelList.style.display = '';
 }
 
-function getGroupItemHTML(key, value) {
+// Crea un item vacio
+function crearBrItemList() {
+    return '<li class="list-group-item"></li>';
+}
+
+// Crea un item de la tabla
+function crearItemList(key, value) {
     let id = key + ' ' + value;
-    return '<a class="list-group-item list-group-item-light list-group-item-action" id="list-' + id + '-list"'
-        + 'data-toggle="list" role="tab" aria-controls="' + id + '">'
+    return '<li class="list-group-item">'
         + '<div class="row no-gutters">'
         + '<div class="col-md-8"><h6>' + id + '</h6></div>'
-        //+ '<div class="col-md-8"><h6>' + id + '</h6></div>'
-        + '<div id="' + id + 'T" class="col-md-4 text-right"><h6>0</h6>'
-        + '</div>'
-        + '</div>'
+        + '<div id="' + id + 'T" class="col-md-4 text-right"><h6>0</h6></div></div>'
         + '<div class="progress">'
         + '<div id="bar' + id + 'T" class="progress-bar bg-warning" role="progressbar" style="width: 0%;" '
-        + 'aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>'
+        + 'aria-valuenow="0" aria-valuemin="0" aria-valuemax="200"></div>'
         + '</div>'
-        + '</a>';
+        + '</li>';
 }
+
+// Crea un item del menu de vistas
+function crearItmeSubMenu(key, value) {
+    let metodo = "clickView('" + key + "','" + value + "')";
+    return '<a class="dropdown-item" href="#" onclick="' + metodo + '">' + value + '</a>';
+}
+
+function createBrSubMenu() {
+    return '<div class="dropdown-divider"></div>';
+}
+
+/*function displayEstado(boolean) {
+    let ver = 'none';
+    let alto = '580px';
+
+    if (boolean) {
+        ver = '';
+        alto = '530px';
+    }
+
+    estadoItemList.style.display = ver;
+    allList.style.height = alto;
+}*/
